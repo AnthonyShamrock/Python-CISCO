add8188f1fb983e06024b44da85a4e95a43e84ba
AnthonyShamrock <72403570+AnthonyShamrock@users.noreply.github.com>
2023-01-28 16:30:10 -0500

    Create main.py

---
+++ First-Year/1-27-2023/main.py
@@ -0,0 +1,12 @@
+numOfHours = 2 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+
+print("Hours: ", numOfHours) #printing the number of hours
+print("Seconds in hours:", numOfHours * seconds)
+print("Goodbye...")
+numOfHours += 1
+print("Seconds in hours:", numOfHours * seconds)
+
+print("Cause I love to under comment, I made an module to add comments for me!")
+import addcomment
+addcomment.run("main.py")
