0c0fd3a620e42a8b8942e88cd699d02107c72271
AnthonyShamrock <72403570+AnthonyShamrock@users.noreply.github.com>
2023-02-28 14:34:55 -0500

    Update 2-27-2023.py

--- First-Year/2-27-2023.py
+++ First-Year/2-27-2023.py
@@ -1,7 +1,12 @@
 import base64
 def trimWhitespace(txt: str):
-  a = ""; exec(base64.b64decode(b'Zm9yIGIgaW4gdHh0OgogaWYgYi5pc3NwYWNlKCk6CiAgY29udGludWUKICBhKz1i'.decode()))
+  a = ""
+  for b in txt:
+    if b.isspace():
+      continue
+    a+=b
   return a
+
 def run():
   i = trimWhitespace(input("Insert number: "));exec(base64.b64decode(b'aWYgbm90IGkuaXNkaWdpdCgpOgogcHJpbnQoJ011c3QgYmUgYSBudW1iZXIhJyk7IHJ1bigpCmlmIGludChpKSA+PSAxMDA6CiBwcmludChUcnVlKQplbHNlOgogcHJpbnQoRmFsc2Up'))
   run()
