7717535eab403bc7ca8312684013d18b3166fafa
AnthonyShamrock <72403570+AnthonyShamrock@users.noreply.github.com>
2023-02-28 14:15:28 -0500

    Create 2-27-2023.py

---
+++ First-Year/2-27-2023.py
@@ -0,0 +1,8 @@
+import base64
+def trimWhitespace(txt: str):
+  a = ""; exec(base64.b64decode(b'Zm9yIGIgaW4gdHh0OgogaWYgYi5pc3NwYWNlKCk6CiAgY29udGludWUKICBhKz1i'.decode()))
+  return a
+def run():
+  i = trimWhitespace(input("Insert number: "));exec(base64.b64decode(b'aWYgbm90IGkuaXNkaWdpdCgpOgogcHJpbnQoJ011c3QgYmUgYSBudW1iZXIhJyk7IHJ1bigpCmlmIGludChpKSA+PSAxMDA6CiBwcmludChUcnVlKQplbHNlOgogcHJpbnQoRmFsc2Up'))
+  run()
+run()
