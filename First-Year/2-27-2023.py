5002e5a51f10ee920d4d3dc51667b9d798cab87c
AnthonyShamrock <72403570+AnthonyShamrock@users.noreply.github.com>
2023-02-28 19:27:35 -0500

    Update 2-27-2023.py

--- First-Year/2-27-2023.py
+++ First-Year/2-27-2023.py
@@ -1,4 +1,8 @@
 import base64
+exec(base64.b64decode(b'ZGVmIHRyaW1XaGl0ZXNwYWNlKHR4dDogc3RyKToKICBhID0gIiIKICBmb3IgYiBpbiB0eHQ6CiAgICBpZiBiLmlzc3BhY2UoKToKICAgICAgY29udGludWUKICAgIGErPWIKICByZXR1cm4gYQoKZGVmIHJ1bigpOgogIGkgPSB0cmltV2hpdGVzcGFjZShpbnB1dCgiSW5zZXJ0IG51bWJlcjogIikpO2V4ZWMoYmFzZTY0LmI2NGRlY29kZShiJ2FXWWdibTkwSUdrdWFYTmthV2RwZENncE9nb2djSEpwYm5Rb0owMTFjM1FnWW1VZ1lTQnVkVzFpWlhJaEp5azdJSEoxYmlncENtbG1JR2x1ZENocEtTQStQU0F4TURBNkNpQndjbWx1ZENoVWNuVmxLUXBsYkhObE9nb2djSEpwYm5Rb1JtRnNjMlVwJykpCiAgcnVuKCkKcnVuKCkK'.decode()))
+
+## OLD WAY ##
+import base64
 def trimWhitespace(txt: str):
   a = ""
   for b in txt:
