9f6aa93765e6c93032b0e988331f5f6a75102638
AnthonyShamrock <72403570+AnthonyShamrock@users.noreply.github.com>
2023-03-03 10:21:00 -0500

    Update 2-27-2023.py

--- First-Year/2-27-2023.py
+++ First-Year/2-27-2023.py
@@ -1,3 +1,15 @@
+find = {
+  True: lambda x: print(int(x) >= 100),
+  False: lambda x: print("MUST BE A DIGIT")
+}
+
+def run():
+  i = input("Insert number: ").strip()
+  find[i.isdigit()](i)
+  run()
+run()
+
+## FUNNY OLD WAY ##
 import base64
 exec(base64.b64decode(b'ZGVmIHRyaW1XaGl0ZXNwYWNlKHR4dDogc3RyKToKICBhID0gIiIKICBmb3IgYiBpbiB0eHQ6CiAgICBpZiBiLmlzc3BhY2UoKToKICAgICAgY29udGludWUKICAgIGErPWIKICByZXR1cm4gYQoKZGVmIHJ1bigpOgogIGkgPSB0cmltV2hpdGVzcGFjZShpbnB1dCgiSW5zZXJ0IG51bWJlcjogIikpO2V4ZWMoYmFzZTY0LmI2NGRlY29kZShiJ2FXWWdibTkwSUdrdWFYTmthV2RwZENncE9nb2djSEpwYm5Rb0owMTFjM1FnWW1VZ1lTQnVkVzFpWlhJaEp5azdJSEoxYmlncENtbG1JR2x1ZENocEtTQStQU0F4TURBNkNpQndjbWx1ZENoVWNuVmxLUXBsYkhObE9nb2djSEpwYm5Rb1JtRnNjMlVwJykpCiAgcnVuKCkKcnVuKCkK'.decode()))
 
